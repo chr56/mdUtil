33ba0bc14309c4bbc03609f9a091843c8356ad63
chr56 <_>
2022-04-07 14:17:34 +0800

    [Repo] init
    import from Phonograph_Plus

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    val kotlinVersion by extra { "1.6.10" }
+    repositories {
+        mavenCentral()
+        google()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+        maven(url = "https://jitpack.io")
+    }
+}
+tasks.create(name = "clean", type = Delete::class) {
+    delete(rootProject.buildDir)
+}

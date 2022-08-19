bd839d05018dbd57c6ba91d7cf97c5ffc918f032
chr56 <_>
2022-08-19 20:47:38 +0800

    [MaterialColorHelper] convert to kotlin

---
+++ mdUtil/src/main/java/util/mddesign/util/MaterialColorHelper.kt
@@ -0,0 +1,47 @@
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+@file:JvmName("MaterialColorHelper")
+
+package util.mddesign.util
+
+import android.content.Context
+import androidx.annotation.ColorInt
+import androidx.appcompat.R
+import androidx.core.content.ContextCompat
+
+@ColorInt
+fun getPrimaryTextColor(context: Context, dark: Boolean): Int {
+    return if (dark) {
+        ContextCompat.getColor(context, R.color.primary_text_default_material_light)
+    } else {
+        ContextCompat.getColor(context, R.color.primary_text_default_material_dark)
+    }
+}
+
+@ColorInt
+fun getSecondaryTextColor(context: Context, dark: Boolean): Int {
+    return if (dark) {
+        ContextCompat.getColor(context, R.color.secondary_text_default_material_light)
+    } else {
+        ContextCompat.getColor(context, R.color.secondary_text_default_material_dark)
+    }
+}
+
+@ColorInt
+fun getPrimaryDisabledTextColor(context: Context, dark: Boolean): Int {
+    return if (dark) {
+        ContextCompat.getColor(context, R.color.primary_text_disabled_material_light)
+    } else {
+        ContextCompat.getColor(context, R.color.primary_text_disabled_material_dark)
+    }
+}
+
+@ColorInt
+fun getSecondaryDisabledTextColor(context: Context, dark: Boolean): Int {
+    return if (dark) {
+        ContextCompat.getColor(context, R.color.secondary_text_disabled_material_light)
+    } else {
+        ContextCompat.getColor(context, R.color.secondary_text_disabled_material_dark)
+    }
+}

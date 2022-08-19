6b27710823972e19028486f924ae19ed060f596f
chr56 <_>
2022-08-19 20:49:10 +0800

    [ToolbarColorUtil] convert to kotlin

---
+++ mdUtil/src/main/java/util/mddesign/util/ToolbarColorUtil.kt
@@ -0,0 +1,28 @@
+@file:JvmName("ToolbarColorUtil")
+
+package util.mddesign.util
+
+import android.content.Context
+import androidx.annotation.CheckResult
+import androidx.annotation.ColorInt
+import util.mdcolor.ColorUtil
+
+@CheckResult
+@ColorInt
+fun toolbarContentColor(context: Context, @ColorInt toolbarColor: Int): Int {
+    return if (ColorUtil.isColorLight(toolbarColor)) {
+        toolbarSubtitleColor(context, toolbarColor)
+    } else {
+        toolbarTitleColor(context, toolbarColor)
+    }
+}
+
+@CheckResult
+@ColorInt
+fun toolbarSubtitleColor(context: Context, @ColorInt toolbarColor: Int) =
+    getSecondaryTextColor(context, ColorUtil.isColorLight(toolbarColor))
+
+@CheckResult
+@ColorInt
+fun toolbarTitleColor(context: Context, @ColorInt toolbarColor: Int) =
+    getPrimaryTextColor(context, ColorUtil.isColorLight(toolbarColor))

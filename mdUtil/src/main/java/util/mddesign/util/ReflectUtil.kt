d72417f4500ffbd9a12c7d1b74e3169f623b8ed8
chr56 <_>
2022-08-20 13:07:32 +0800

    [ReflectUtil] simplify reflectField

--- mdUtil/src/main/java/util/mddesign/util/ReflectUtil.kt
+++ mdUtil/src/main/java/util/mddesign/util/ReflectUtil.kt
@@ -3,8 +3,8 @@ package util.mddesign.util
 import kotlin.jvm.Throws
 
 @Throws(NoSuchFieldException::class, SecurityException::class)
-internal inline fun <T, reified F> T.reflectField(clazz: Class<T>, fieldName: String): F {
-    val f = clazz.getDeclaredField(fieldName)
+internal inline fun <reified T, reified F> T.reflectDeclaredField(fieldName: String): F {
+    val f = T::class.java.getDeclaredField(fieldName)
     f.isAccessible = true
     return f.get(this) as F
 }
